f6093d2c53003981a65b4c1f00d0b3a566109fa0
Chas Huggins <hugginsc10@gmail.com>
2020-02-29 13:57:15 -0500

    fixed request

--- app.py
+++ app.py
@@ -1,9 +1,5 @@
-from flask import Flask
-from learning import learning_blueprint
+import requests
 
-app = Flask(__name__)
+response = requests.get("https://www.bestbuy.com/site/apple-macbook-pro-16-display-with-touch-bar-intel-core-i9-16gb-memory-amd-radeon-pro-5500m-1tb-ssd-latest-model-space-gray/6366572.p?skuId=6366572")
 
-app.register_blueprint(learning_blueprint, url_prefix='/greetings')
-
-if __name__ == '__main__':
-  app.run()
+print(response.content)
