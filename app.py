d8f0ca56423f7d7faec56bc20b34cef16c4d47f4
Chas Huggins <hugginsc10@gmail.com>
2020-02-29 13:40:47 -0500

    remove .vscode and added requirements.txt

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask
+from learning import learning_blueprint
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-  return"Hello World"
-
+app.register_blueprint(learning_blueprint, url_prefix='/greetings')
 
 if __name__ == '__main__':
   app.run()
